beadf2f8095928b70873d3bef494850fdcdcbdec
sanjidatanha09 <tanha.sanjida999@gmail.com>
2023-11-06 10:13:00 +0600

    download image from imgbb and update all image

--- src/Home/Section2.jsx
+++ src/Home/Section2.jsx
@@ -5,10 +5,10 @@ const Section2 = () => {
         <div className='lg:flex justify-center items-center mb-36'>
             <div className='lg:w-[50%] relative'>
                 <div className='lg:w-3/4 '>
-                    <img className='w-full' src="https://i.ibb.co/sKpLB76/Bull-Bell-Steakhouse-CREDITDestination-Riverina-Murray.jpg" alt="" />
+                    <img className='w-full my-5 lg:my-0' src="https://i.ibb.co/sKpLB76/Bull-Bell-Steakhouse-CREDITDestination-Riverina-Murray.jpg" alt="" />
 
                 </div>
-                <div className='lg:w-3/5 absolute top-52 left-48 '>
+                <div className='hidden lg:flex lg:w-3/5 absolute top-52 left-48 '>
                     <img className='w-full rounded-full' src="https://i.ibb.co/VQJSF22/tiny-people-standing-near-box-donation-food-delivery-volunteers-giving-healthy-grocery-goods-charity.jpg" alt="" />
 
                 </div>
@@ -16,7 +16,7 @@ const Section2 = () => {
                 
                 
             </div>
-            <div className='lg:w-[50%] text-lg font-bold text-gray-700'>
+            <div className='lg:w-[50%] text-sm md:text-lg lg:text-lg font-bold text-gray-700'>
                 <h1>Food is not just a necessity; it's a celebration of flavors, cultures, and traditions that bring people together. From the sizzling stir-fries of Asia to the hearty stews of Europe, the world's cuisines offer a delightful journey for the taste buds.</h1>
                 <h2>Food donation is a noble act that can transform lives, providing sustenance to those in need. Many charitable organizations rely on food donations to support the hungry and underprivileged individuals in our communities.</h2>
             </div>
