6c6c04749c273c39cec38418302bee41e0891dbf
Sanjida Akter <sanjidatanha09@gmail.com>
2024-04-14 23:00:02 +0600

    again space removed from other component

--- src/Pages/AvailableFood/AvailableFood2.jsx
+++ src/Pages/AvailableFood/AvailableFood2.jsx
@@ -13,38 +13,24 @@ const AvailableFood2 = ({ availableFood }) => {
         const sortData=() =>{
             dateList.sort((a,b) =>new Date(a.date) - new Date(b.date))
             console.log(dateList)
-            setSortList();
-
-            
+            setSortList(); 
         }
         sortData()
-        
-
     }, [])
   
-
-
     return (
         <div>
             <div className='text-center font-bold md:text-2xl lg:text-3xl mb-5'>
                 {/* Total Available Food:{fullCampaignes.length} */}
             </div>
-            
-
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 px-2  lg:px-0'>
                 {
                     availableFood?.map(food => <AvailableFoodCard
                         key={food._id}
                         food={food}
-
-
                     ></AvailableFoodCard>)
                 }
             </div>
-
-
-
-
         </div>
     );
 };
--- src/Pages/AvailableFood/AvailableFoodCard.jsx
+++ src/Pages/AvailableFood/AvailableFoodCard.jsx
@@ -1,18 +1,8 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../../Providers.jsx/AuthProvider';
-import useAuth from '../../Hooks/useAuth';
 
 const AvailableFoodCard = ({ food}) => {
-
-    const { user } = useAuth();
     const {_id, foodname, foodimage, foodquantity, location, date, additionalnotes, foodstatus, donatorname, donatorimage, donatoremail } =food;
-
-  
-
-  
-
-
     return (
         <div>
             <div className="card  mx-auto bg-base-100 shadow-xl ">
@@ -20,16 +10,11 @@ const AvailableFoodCard = ({ food}) => {
                 <div className="card-body">
                     <h2 className="card-title lg:text-3xl mt-0">
                     {foodname}
-
                     </h2>
-
                     <div className='flex justify-between items-center '>
                         <p className='text-gray-800 text-sm font-bold '>Quantity : {foodquantity}</p>
                         <p className='text-gray-600 text-sm font-bold text-end'>Exp Date :{date} </p>
-
                     </div>
-
-
                     <p className='h-[140px] text-gray-500 text-base'>{additionalnotes}</p>
                     <div className='flex justify-between items-center'>
                         <div className='flex justify-center items-center gap-2'>
@@ -39,16 +24,12 @@ const AvailableFoodCard = ({ food}) => {
                         <div>
                             <h1 className='font-bold text-gray-700'>{location}</h1>
                         </div>
-
                     </div>
                     <div className="card-actions justify-end">
                         <Link to={`/singlefood/${_id}`}>
                             <button className='btn btn-warning text-white font-bold'>Details</button>
                         </Link>
-
                     </div>
-
-                  
                 </div>
             </div>
         </div>
