34f79f190e4e59253664adf54bee8f92ec6520b8
Sanjida Akter <sanjidatanha09@gmail.com>
2024-04-14 23:13:11 +0600

    space removed

--- src/Pages/ManageFood/Button/Button.jsx
+++ src/Pages/ManageFood/Button/Button.jsx
@@ -4,40 +4,34 @@ import Button2 from './Button2';
 
 const Button = () => {
     const buttonupdate = useLoaderData();
-   
-
     const [requestfoods, setRequestFood] = useState(buttonupdate);
-    
-    const handleConfirm = id =>{
-        fetch(`https://assignment-11-server-smoky-mu.vercel.app/requfood/${id}`,{
+    const handleConfirm = id => {
+        fetch(`https://assignment-11-server-smoky-mu.vercel.app/requfood/${id}`, {
             method: 'PATCH',
-            headers :{
-                'content-type':'application/json'
+            headers: {
+                'content-type': 'application/json'
             },
-            body:JSON.stringify({status: 'confirm'})
+            body: JSON.stringify({ status: 'confirm' })
 
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            if(data.modifiedCount > 0){
-                //update
-                const remaining = requestfoods.filter(requestfood => requestfood._id !==id);
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    //update
+                    const remaining = requestfoods.filter(requestfood => requestfood._id !== id);
+                    const updated = requestfoods.find(requestfood => requestfood._id === id);
+                    updated.status = 'confirm'
+                    const newFoodie = [updated, ...remaining];
+                    setRequestFood(newFoodie);
 
-                const updated = requestfoods.find(requestfood =>requestfood._id ===id);
-                updated.status = 'confirm'
-                const newFoodie = [updated, ...remaining];
-                setRequestFood(newFoodie);
-
-            }
-        })
+                }
+            })
     }
-    
-
 
     return (
         <div>
-           <h1>{requestfoods.length}</h1>
+            <h1>{requestfoods.length}</h1>
 
             <div className="overflow-x-auto mt-10">
                 <table className="text-center table table-xs table-pin-rows table-pin-cols">
@@ -53,11 +47,10 @@ const Button = () => {
                             <th>Requester Image</th>
 
                             <th>Requester Email
-</th>
-                            
+                            </th>
                             <th>Request Time and Date</th>
                             <th>status</th>
-                            
+
                         </tr>
                     </thead>
                     <tbody className=''>
@@ -66,7 +59,7 @@ const Button = () => {
                             requestfoods.map(requestfood => <Button2
                                 key={requestfood._id}
                                 requestfood={requestfood}
-                                handleConfirm = {handleConfirm}
+                                handleConfirm={handleConfirm}
 
                             ></Button2>)
                         }
